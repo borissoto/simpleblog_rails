2f1795dbaffd55459a5f04be1365c335fccd032b
Borrison <borissoto@gmail.com>
2020-09-30 13:37:33 -0400

    using function resources on routes for generate crud-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #root
   root 'posts#index'
+  #regular route
+  get 'about' => 'pages#about'
+
+  #resource route
+  resources :posts
 end
