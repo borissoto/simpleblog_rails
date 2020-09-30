2d281bc930358d2ad6bcf58ad44fc8c5681f78a5
Borrison <borissoto@gmail.com>
2020-09-30 15:06:33 -0400

    adding  bootstrap and validate post title lenght to minimun 5 in model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #root
-  root 'posts#index'
+  root 'posts#index', as: 'home'
   #regular route
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about', as: 'about'
 
   #resource route
   resources :posts
